ac11fe7771e16f854c55ab0abad01c2449cffcb5
Durga Prasad Vasupalli <durgaprasadvasupalli07@gmail.com>
2026-08-13 09:31:00 +0530

    Add files via upload

---
+++ const.js
@@ -0,0 +1,16 @@
+
+const number =30;
+console.log(number);
+document.writeln(number);
+let numbers =23.7;
+console.log(numbers);
+document.writeln(numbers);
+let number1 =100;
+console.log(number1);
+document.writeln(number1);
+let number2 ="KLU";
+console.log(number2);
+document.writeln(number2);
+let number3 ="KLEF";
+console.log(number3);
+document.writeln(number3);
